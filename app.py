2ee6cd6ef1745f2ad5e93d177a33fe9916998cb4
Nithya <nithyakrish@gmail.com>
2023-10-09 14:15:09 +0800

    Updated header

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if "memory" not in st.session_state.keys():
 # Initialize the chat message history
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about the following documents!"}
+        {"role": "assistant", "content": "Ask me a question from the above documents!"}
     ]
     st.write(" 1. MU 72/19 (17 SEP 2019) - LIA GUIDELINES ON THE USE OF INCENTIVES IN THE RECRUITMENT OF FINANCIAL ADVISORY REPRESENTATIVES\n")
     st.write(" 2. Notice FAAN13 Minimum Entry and Examination Requirements for Representatives of Licensed Financial A\n")
